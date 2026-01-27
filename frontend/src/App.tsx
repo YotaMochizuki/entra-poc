a69f7c9d22b1cca2667ce5f65bf2d3e99cc5b314
YotaMochizuki <mochizuki1237.2012@gmail.com>
2026-01-27 21:03:08 +0000

    frontend開発環境調整

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1>React + Actix DevContainer</h1>;
+	return <h1>React + Actix DevContainer</h1>;
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById("root");
+if (!root) {
+	throw new Error('Root element "#root" not found');
+}
+
+ReactDOM.createRoot(root).render(<App />);
